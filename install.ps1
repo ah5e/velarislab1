433f13789556b19782772943e900c827d2d8dff4
ah5e <wwfawaz220@gmail.com>
2026-03-27 16:51:13 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,14 +3,13 @@ Write-Host "VELARIS INSTALL STARTED..." -ForegroundColor Cyan
 $zip = "$env:TEMP\velaris.zip"
 $folder = "$env:TEMP\VELARIS"
 
-Write-Host "Downloading..." -ForegroundColor Yellow
-
 Invoke-WebRequest "https://github.com/ah5e/velarislab1/releases/download/v1.0/VELARIS.zip" -OutFile $zip
-
-Write-Host "Extracting..." -ForegroundColor Yellow
-
 Expand-Archive -Path $zip -DestinationPath $folder -Force
 
-Write-Host "Launching..." -ForegroundColor Green
+$exe = Get-ChildItem $folder -Recurse -Filter "*.exe" | Select-Object -First 1
 
-Start-Process (Get-ChildItem $folder -Recurse -Filter "Velaris.exe" | Select-Object -First 1).FullName
+if ($exe) {
+    Start-Process $exe.FullName
+} else {
+    Write-Host "ERROR: No EXE found inside ZIP" -ForegroundColor Red
+}
