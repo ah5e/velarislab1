14726e4525a8bea472f38ef20dcf21f11eac2d9a
ah5e <wwfawaz220@gmail.com>
2026-03-27 16:46:45 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,16 @@
+Write-Host "VELARIS INSTALL STARTED..." -ForegroundColor Cyan
+
 $zip = "$env:TEMP\velaris.zip"
 $folder = "$env:TEMP\VELARIS"
 
-Invoke-WebRequest "https://raw.githubusercontent.com/ah5e/velarislab/main/VELARIS.zip" -OutFile $zip
+Write-Host "Downloading..." -ForegroundColor Yellow
+
+Invoke-WebRequest "https://github.com/ah5e/velarislab1/releases/download/v1.0/VELARIS.zip" -OutFile $zip
+
+Write-Host "Extracting..." -ForegroundColor Yellow
 
 Expand-Archive -Path $zip -DestinationPath $folder -Force
+
+Write-Host "Launching..." -ForegroundColor Green
 
 Start-Process "$folder\Velaris.exe"
